338ecc74604e303a5ac94b89e9fc4aac1c805bdb
MazzGuille <mazzcode@gmail.com>
2022-06-01 00:58:22 -0300

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Windows;
+
+namespace ConexcionGestionPedidos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string MiConexion = ConfigurationManager.ConnectionStrings["ConexcionGestionPedidos.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
+        }
+    }
+}
